447fdd6ba9dd5f30687fcadd5a0bcc1cbfc4be78
taw <tawshif27@gmail.com>
2019-03-17 23:30:58 -0700

    delete remaining starter comment

--- js/script.js
+++ js/script.js
@@ -122,4 +122,3 @@ function printQuote(){
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
